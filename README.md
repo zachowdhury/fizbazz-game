32b12b0281602bf4e65776a4bad4ef73f865e2fe
Zahid A. Chowdhury <zahid@iotek.fi>
2018-07-25 17:39:54 +0300

    simply nice test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chai-mocha-simple
 A basic test Project
+install mocha and chai
+# npm install -g mocha --save 
+# npm install chai --save
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var expect = require ( 'chai').expect;
+
+describe ('Simple test ', function (){
+    it('Should run Simple and pass', function (){
+        expect(true).to.be.true ;
+
+    });
+});
